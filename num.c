7666411bfc312e03d4a373e7169fdfcbc002d8c5
Fengming Sun <s@sfmblog.cn>
2017-12-03 15:52:30 +0800

    optimize

--- num.c
+++ num.c
@@ -1,38 +1,35 @@
 #include <stc12.h>
 
 /**
- * For 12MHz 晶振
- * 硬件时钟 1/1000000
- * 10ms = 10000 个硬件时钟
- *
- * 定时周期 1ms 为 1000 个硬件时钟
+ * For 11.0592MHz 晶振
+ * 硬件时钟 12/11059200
+ * 
+ * 12x/11059200 = 0.001
+ * 12x = 11059.2
+ * x = 11059.2 / 12 
+ * x = 921
  */
 
 unsigned int cnt = 0;
 unsigned char __code chars[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
-unsigned char buffer[4] = { 0x00, 0x00, 0x00, 0x00 };
+unsigned char buffer[4] = {0x00, 0x00, 0x00, 0x00};
+unsigned char T0RH, T0RL;
+
+void configTimer0(unsigned int ms);
 
 void main()
 {
     unsigned long sec = 0;
-    TMOD = 0x01;
-    TR0 = 1;
-    ET0 = 1; // T0 中断开关
-    EA = 1; // 中断使然总开关
+    EA = 1;  // 中断使然总开关
+    configTimer0(1); // 1ms 中断
 
-    TH0 = 0xFC;
-    TL0 = 0x18;
-
-    P2_4 = 0;
-    P2_5 = 0;
-    P2_6 = 0;
-    P2_7 = 0;
-
+    P2 &= 0x0F; // 低4位不变， 高4位清零
     P1 = 0xFE;
 
     while (1)
     {
-        if (cnt >= 1000) {
+        if (cnt >= 1000)
+        {
             cnt = 0;
             sec++;
             P1 = ~(0x01 << (sec % 8));
@@ -44,22 +41,37 @@ void main()
     }
 }
 
-void interruptTimer0() __interrupt (1) {
+void configTimer0(unsigned int ms)
+{
+    unsigned long tmp;
+    tmp = 11059200 / 12; // 定时器计数频率
+    tmp = (tmp * ms) / 1000; // 计算所需数值
+    tmp = 65536 - tmp; // 计算定时器重载值
+    tmp = tmp + 18; // 补偿误差
+
+    TMOD &= 0xF0;
+    TMOD |= 0x01;
+
+    T0RH = (unsigned char)(tmp >> 8); // 高8位 
+    T0RL = (unsigned char)tmp; // 低8位
+
+    TH0 = T0RH;
+    TL0 = T0RL;
+    ET0 = 1;
+    TR0 = 1;
+    
+}
+
+void interruptTimer0() __interrupt(1)
+{
     static unsigned char i = 0;
 
-    TH0 = 0xFC;
-    TL0 = 0x18;
+    TH0 = T0RH;
+    TL0 = T0RL;
     cnt++;
 
-    P2_4 = 0;
-    P2_5 = 0;
-    P2_6 = 0;
-    P2_7 = 0;
-    P0 = buffer[i++];
-    switch (i - 1) {
-        case 0: P2_4 = 1; break;
-        case 1: P2_5 = 1; break;
-        case 2: P2_6 = 1; break;
-        case 3: P2_7 = 1; i = 0; break;
-    }
+    P2 &= 0x0F;
+    P0 = buffer[i];
+    P2 |= 0x10 << i; // 高位位移
+    i = ++i & 0x03; // i = 0 ... 3
 }
